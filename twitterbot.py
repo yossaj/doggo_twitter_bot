9d77724d69f4b7f964ef03db3b581a9cc701e912
yossaj <yossaj@icloud.com>
2019-07-01 20:41:39 +0100

    inital commit

---
+++ twitterbot.py
@@ -0,0 +1,12 @@
+import tweepy
+
+print('this is my twitter bot')
+
+CONSUMER_KEY = 'v1DkBLPjtACtvWwCABworreNi',
+CONSUMER_SECRET = '35qhW8bzlPUMHVGOQCfXvJRgreHKujvnOXjF5XcXOPF9tW6lyN'
+ACCESS_KEY = '1144955407028408321-rcB279KkouRHteW7kggXjtF9V35vqW'
+ACCESS_SECRET = 'IPIagFYVIxdFF3wL2Y4kC4HBD63b1TpiYQVs8u6NC0NTT'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
